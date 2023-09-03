7636b302585acce0141740783c70e145d1c4bfa2
= <tominoboda@gmail.com>
2023-09-03 19:46:31 +0200

    chore(code): cleaned code

--- src/main/java/com/app/main/Main.java
+++ src/main/java/com/app/main/Main.java
@@ -1,9 +1,6 @@
 package com.app.main;
 
 import com.app.api.Api;
-import com.app.model.*;
-import com.app.utils.datetime.DateUtil;
-import com.app.utils.Generator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -21,7 +18,6 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Api.connect();
-        //initializeMockedData();
 
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/views/layout.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
@@ -36,40 +32,6 @@ public class Main extends Application {
         stage.setMinWidth(MIN_WIDTH);
         stage.setMinHeight(MIN_HEIGHT);
         stage.show();
-    }
-
-    private void initializeMockedData() {
-        String emp1Id = Generator.getId();
-        String emp2Id = Generator.getId();
-        String emp3Id = Generator.getId();
-        String emp4Id = Generator.getId();
-        String emp5Id = Generator.getId();
-
-        Api.employees().add(new Employee(emp1Id, "John Doe", "johndoe@email.com", Role.SOFTWARE_DEVELOPER, EmploymentType.FULL_TIME, 4200));
-        Api.employees().add(new Employee(emp2Id, "Jack Apple", "jackapple@email.com", Role.PRODUCT_OWNER, EmploymentType.FULL_TIME, 3800));
-        Api.employees().add(new Employee(emp3Id, "Marry Johnson", "marryjohnson@email.com", Role.SOFTWARE_TESTER, EmploymentType.INTERNSHIP, 2200));
-        Api.employees().add(new Employee(emp4Id, "Eric Wooden", "ericwooden@email.com", Role.PRODUCT_OWNER, EmploymentType.PART_TIME, 1900));
-        Api.employees().add(new Employee(emp5Id, "Mayhem Brown", "mayhembrown@email.com", Role.SOFTWARE_DEVELOPER, EmploymentType.CONTRACT, 2300));
-
-        String team1Id = Generator.getId();
-        String team2Id = Generator.getId();
-
-        Api.teams().add(new Team(team1Id, "RSU"));
-        Api.teams().add(new Team(team2Id, "IAM"));
-
-        String proj1id = Generator.getId();
-        String proj2id = Generator.getId();
-
-        Api.projects().add(new Project(proj1id, team1Id, "RSU", "Remote Software Update Tool for Siemens Mobility", 2000, DateUtil.toDate(1, 1, 2023), DateUtil.toDate(15, 10, 2023)));
-        Api.projects().add(new Project(proj2id, team2Id, "IAM", "User Management Tool for Siemens Mobility", 3000, DateUtil.toDate(15, 3, 2023), DateUtil.toDate(15, 8, 2024)));
-
-        Api.teams().addEmployeeToTeam(team1Id, emp1Id);
-        Api.teams().addEmployeeToTeam(team1Id, emp2Id);
-        Api.teams().addEmployeeToTeam(team1Id, emp3Id);
-        Api.teams().addEmployeeToTeam(team2Id, emp4Id);
-        Api.teams().addEmployeeToTeam(team2Id, emp5Id);
-
-        Api.tickets().add(new Ticket(Generator.getId(), proj1id, "Fixed loading bug", "Fixes a loading bug in lazy loading", TicketStatus.IN_PROGRESS, 10, emp1Id, emp2Id, emp3Id));
     }
 
     public static void main(String[] args) {
